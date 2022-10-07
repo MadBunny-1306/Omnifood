8bf0aee6b19f6e1d83f4a3d5c5c1c2c9ab736d41
Slady <csl.bunny.2207@gmail.com>
2022-10-07 19:11:35 +0200

    Implementing a Sticky Navigation Bar

--- js/script.js
+++ js/script.js
@@ -63,6 +63,31 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+// ///////////////////////////////////
+// Making navigation sticky
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //  In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
